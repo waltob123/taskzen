9174f91134dacae5912ba0d1c5b63d91e856819a
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2024-05-30 08:52:46 +0100

    Subject: (modify) add task component.
    Body:
    - surrounded input and button with form tags
    - used the onSubmit to handle adding a task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,22 +4,35 @@ const AddTask = ({ tasksArray, addNewTask }) => {
 
   const [taskInput, setTaskInput] = useState('');
 
-  const handleAddTask = () => {
+  const handleAddTask = (e) => {
+    e.preventDefault();
     if (taskInput.trim() === '') {
       alert('Task cannot be empty');
     } else {
-      const newTask = { id: tasksArray.length + 1, task: taskInput.trim() };
-      addNewTask(newTask);
+      const newTask = { task: taskInput.trim() };
+      // addNewTask(newTask);
+      // console.log(newTask);
+      fetch('http://localhost:8000/tasks', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTask)
+      }).then(() => {
+        // addNewTask(newTask);
+        console.log('Added task successfully!');
+      })
       setTaskInput('');
     }
   }
 
   return (
     <>
-    <section className="add-task">
-        <input type="text" name="task" id="task-input" className="form-input"
-        value={taskInput} onChange={(e) => setTaskInput(e.target.value)} />
-        <button className="btn btn-submit" onClick={handleAddTask}>Add Task</button>
+      <section className="add-task">
+        <form action="" method="post"
+        onSubmit={(e) => handleAddTask(e)}>
+          <input type="text" name="task" id="task-input" className="form-input"
+            value={taskInput} onChange={(e) => setTaskInput(e.target.value)} />
+          <button className="btn btn-submit">Add Task</button>
+        </form>
     </section>
     </>
   );
